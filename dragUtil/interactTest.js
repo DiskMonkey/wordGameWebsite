968f4b12d0919c3a8966445f362de414bc894de6
DiskMonkey <MukulPlace@gmail.com>
2023-11-16 11:01:12 -0500

    add link to interact server download

--- dragUtil/interactTest.js
+++ dragUtil/interactTest.js
@@ -1,12 +1,10 @@
-import interact from '../node_modules/interactjs'
-
 const boogie = interact('.boogie')
 boogie.draggable({
 	origin: 'self',                   // (0, 0) will be the element's top-left
     inertia: true,                    // start inertial movement if thrown
     listeners: {
 		move(event){
-			print("moved")
+			console.log("moved")
 		}
 	}
 })
