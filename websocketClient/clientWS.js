485f287d5ff2b2dee3056ffdd997a076ec638261
Mukul Ramesh <mukulplace@gmail.com>
2024-01-04 16:24:12 -0500

    changed ip

--- websocketClient/clientWS.js
+++ websocketClient/clientWS.js
@@ -2,7 +2,7 @@ function initWebSocket()
 {
 	return new Promise(function (resolve, reject)
 	{
-		var ws = new WebSocket('ws://3.91.255.112:5050');
+		var ws = new WebSocket('ws://ec2-3-91-255-112.compute-1.amazonaws.com:5050');
 		ws.onopen = () =>
 		{
 			console.log('Connection opened!');
